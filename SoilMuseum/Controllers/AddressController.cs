ca36e19c84a57223c20ea7e7844577a56982106c
Hridoy Roy <43823404+hridoy335@users.noreply.github.com>
2019-12-29 10:04:07 +0600

    Working Location. Dividion Insert Done

---
+++ SoilMuseum/Controllers/AddressController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoilMuseum.Models;
+
+namespace SoilMuseum.Controllers
+{
+    public class AddressController : Controller
+    {
+        Models.SoilMuseumEntities1 db = new SoilMuseumEntities1();
+        // GET: Address
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Division Insert action ****
+        [HttpGet]
+        public ActionResult Division()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Division([Bind(Include = "Division_ID,Division_Name")] Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Divisions.Add(division);
+                db.SaveChanges();
+                return RedirectToAction("DivisionList");
+            }
+            return View(division);
+        }
+
+        // Division List ***
+        public ActionResult DivisionList()
+        {
+            return View(db.Divisions.ToList());
+        }
+    
+    }
+}
